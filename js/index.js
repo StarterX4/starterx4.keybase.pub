fa3994f81a7609afdfab270f9b9980ea975183a6
StarterX4 <starterx4@gmail.com>
2021-11-16 17:32:58 +0100

    Update index.js
    remove annoying welcome monit, cleanings, font size test at moghyasays

--- js/index.js
+++ js/index.js
@@ -146,63 +146,11 @@ function onWindowResize(){
 
 $(window).resize(onWindowResize);
 
-var profile;
-swal({
-		title: "Hello World!!!",
-		text: "Hello visitor, you have landed upon little webspace of moghya. I hope you're doing well."
-		// buttons: ["Nope, I'm just looking around.", "Yes, I'm hiring :)"]
-		// buttons: {
-		// 	cancel: {
-		// 	  text: "Nope.",
-		// 	  value: false,
-		// 	  visible: true,
-		// 	  className: "button-cancel",
-		// 	  closeModal: true,
-		// 	},
-		// 	confirm: {
-		// 	  text: "Yes, I'm hiring.",
-		// 	  value: true,
-		// 	  visible: true,
-		// 	  className: "button-confirm",
-		// 	  closeModal: true
-		// 	}
-		// }
-});
-//.then((value)=>{
-// 	if(value===true) {
-// 		swal({
-// 			title: "Hello Talent Scout,",
-// 			text: "Thank you for visiting my webspace. I hope you'll find relevant information here. If you need any other information, kindly reach to me. \n\n Do you need a copy of my resume?",
-// 			buttons: {
-// 				cancel: {
-// 				  text: "I have your resume.",
-// 				  value: false,
-// 				  visible: true,
-// 				  className: "button-cancel",
-// 				  closeModal: true,
-// 				},
-// 				confirm: {
-// 				  text: "Yes, sure.",
-// 				  value: true,
-// 				  visible: true,
-// 				  className: "button-confirm",
-// 				  closeModal: true
-// 				}
-// 			}
-// 		}).then((value)=>{
-// 			if(value===true) { 
-// 				window.open('/Resume_Shubham_Sawant.pdf');
-// 			}
-// 		})
-// 	}
-// });
-
-
 function loadMoghysSays() {
 	const moghyaSaysInnerHtml = `<div class="col m6">
-		<h6>Recipe for this website:</h6>	
+		<h6 font-size=".67em">Recipe for this website:</h6>	
 		<div class="row">
-			Would you like to have your own portfolio in this template ? It"s pretty easy, <a href="https://github.com/moghya">moghya</a> covered it up for everyone out their. 
+			Would you like to have your own portfolio in this template? It"s pretty easy, <a href="https://github.com/moghya">moghya</a> covered it up for everyone out their. 
 			All the content on this website is dynamically loaded from JSON data.
 			Fork this <a href="https://github.com/moghya/moghya.github.io/">repo</a> on github and edit <a href="https://github.com/moghya/moghya.github.io/blob/master/js/profile.json">js/profile.json</a> for adding your data.
 			<a href="https://medium.com/howcatcancode/developer-profile-template-2017-219f43147efe">Read more</a><br>
